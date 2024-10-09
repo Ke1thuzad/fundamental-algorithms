d0f36eb2aeda2aa7e3621a4fd9a57b7f3e84d3a3
Dmitriy <dolbus1235@mail.ru>
2024-10-10 01:24:58 +0300

    2.3 (maybe) done?

--- lab2/task3/handlers.c
+++ lab2/task3/handlers.c
@@ -60,16 +60,89 @@ int shift_table(const char *template, int n, int *result) {
     return 0;
 }
 
-int find_substr_in_files(char* substr, int m, int file_count, ...) {
+int line_counter(const char *str, int size) {
+    int count = 1;
+    for (int i = 0; i < size; ++i) {
+        if (str[i] == '\n')
+            count++;
+    }
+
+    return count;
+}
+
+int pos_in_line(const char *str, int size) {
+    if (str[size - 1] == '\n')
+        return 1;
+    for (int i = size - 2; i >= 0; --i) {
+        if (str[i] == '\n')
+            return size - i - 1;
+    }
+    return size;
+}
+
+int find_substr_in_files(char *substr, int m, int file_count, ...) {
     va_list files;
     char* filename;
 
     va_start(files, file_count);
     for (int i = 0; i < file_count; ++i) {
         filename = va_arg(files, char*);
-        // TODO: Make search
+        int err = file_handler(substr, m, filename);
+        if (err) {
+            va_end(files);
+            return err;
+        }
     }
     va_end(files);
 
     return 0;
 }
+
+int file_handler(char *key, int m, char *filename) {
+    FILE* stream = fopen(filename, "r");
+    if (!stream)
+        return throw_err(FILE_ERROR);
+
+    fseek(stream, 0, SEEK_END);
+    int size = ftell(stream);
+    fseek(stream, 0, SEEK_SET);
+
+    char content[size];
+
+    for (int i = 0; i < size; ++i) {
+        content[i] = (char) fgetc(stream);
+    }
+
+    int i = 0, ind = 1;
+
+    while (1) {
+        i = substr(content + ind, size - ind, key, m);
+        if (i == -1) break;
+        ind += i + 1;
+        printf("Found occurrence in file %s, line %d, character %d\n", filename, line_counter(content, ind),
+               pos_in_line(content, ind));
+
+    }
+
+    fclose(stream);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- lab2/task3/main.c
+++ lab2/task3/main.c
@@ -1,15 +1,7 @@
 #include "main.h"
 
 int main(int argc, char** argv) {
-    char a[] = "lilila", b[] = "mililiters of lililoo are great for lililaloo";
-//    printf("%c\n", b[36]);
-    printf("%d\n", substr(b, len(b), a, len(a)));
-
-//    int n = 10, table[n];
-//    shift_table(a, n, table);
-//    for (int i = 0; i < n; ++i) {
-//        printf("%d ", table[i]);
-//    }
-
+    char sub[] = "lila";
+    find_substr_in_files(sub, len(sub), 2, "a", "b");
     return 0;
 }
