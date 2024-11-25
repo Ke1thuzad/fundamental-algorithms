81f947e2132bea10946b901c2f21ab173dbded81
MrDemmy <66142897+MrDemmy@users.noreply.github.com>
2024-11-26 00:37:35 +0300

    5.6

---
+++ lab5/task6/main.cpp
@@ -0,0 +1,32 @@
+#include "main.h"
+
+int main() {
+    vector a(10, 1);
+//
+    a.insert(15, 3);
+    a.insert(106, 899);
+    a.insert(2, 2);
+    a.insert(2, 2);
+    a.insert(2, 2);
+    a.insert(14, 7);
+    a.insert(16, 8);
+    a.erase(14);
+
+    vector b(a.begin(), a.end());
+//
+//    std::cout << a.pop_back() << '\n';
+//    std::cout << a.pop_back() << '\n';
+//    std::cout << a.pop_back() << '\n';
+//    std::cout << a.pop_back() << '\n';
+//    std::cout << a.pop_back() << '\n';
+
+//    a.resize(222, 4);
+//
+//    a.insert(221, 5);
+
+    std::cout << b << b.size() << '\n' << b.capacity() << '\n' << (a.begin() <= a.end());
+
+    std::cout << a << a.size() << '\n' << a.capacity() << '\n' << a.back();
+
+    return 0;
+}
